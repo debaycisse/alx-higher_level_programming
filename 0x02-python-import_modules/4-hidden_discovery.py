c04175b0b65df8469d869f2319d18ed99f8bea06
Azeez Adebayo <debaycisse@gmail.com>
2024-01-05 07:47:39 -0800

    apply a workarround to ensure items are sorted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    items = []
     for name in dir('hidden_4.pyc'):
+        items.append(name)
+    items.sort()
+    for name in items:
         print(name)
