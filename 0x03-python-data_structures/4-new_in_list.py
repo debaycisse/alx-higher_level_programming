8948cd9f2183ae0297b0fcb3c460cd0a10896e4d
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 04:57:47 -0800

    implement task 4
    replaces element at a given index position
    while retaining the original list and return the new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or type(idx) != int:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    new_l = []
+    for i in range(len(my_list)):
+        if i == idx:
+            new_l.append(element)
+        else:
+            new_l.append(my_list[i])	
+    return new_l
