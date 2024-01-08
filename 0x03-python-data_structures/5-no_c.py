815dfb79e5fe261426f465c17649ae4e90fd762d
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 06:42:41 -0800

    apply workaround to remove c and C via a method

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,12 +2,8 @@ def no_c(my_string):
     if len(my_string) <= 0:
         return my_string
     else:
-        n_str = ""
-        for letter in my_string:
-            if ord(letter) == ord("C"):
-                pass
-            elif ord(letter) == ord("c"):
-                pass
-            else:
-                n_str += letter
-    return n_str
+        c = {ord('c'): None}
+        C = {ord('C'): None}
+        new_str = my_string.translate(c)
+        new_str = new_str.translate(C)
+        return new_str
