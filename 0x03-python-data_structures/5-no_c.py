5f794ce47b3d39b3ad9d1ca3f29d76ec9f024cb4
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 07:28:12 -0800

    handle each element of string via slice

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 def no_c(my_string):
-    if my_string:
+    if my_string[:]:
         new_str = my_string.translate({ord("c"): None})
         new_string2 = new_str.translate({ord("C"): None})
         return new_string2
