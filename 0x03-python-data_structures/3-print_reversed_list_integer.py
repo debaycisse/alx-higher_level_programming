b873741faf4253d76dad0ebd1f0c5c23292249cb
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 03:56:40 -0800

    apply workaround to ensure integer type

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     new_l = [my_list[len(my_list) - (x + 1)] for x in range(len(my_list))]
     for i in range(len(new_l)):
-        print("{}".format(new_l[i]))
+        print("{:d}".format(new_l[i]))
