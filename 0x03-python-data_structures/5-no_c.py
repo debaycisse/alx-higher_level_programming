d1db63e887fb6cf7f2a962b2eed0a4f9b2e41879
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 07:25:54 -0800

    convert file an executable file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     if my_string:
         new_str = my_string.translate({ord("c"): None})
-        new_str2 = new_str.translate({ord("C"): None})
-        return new_str2
+        new_string2 = new_str.translate({ord("C"): None})
+        return new_string2
     return my_string
