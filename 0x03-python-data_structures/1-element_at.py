ef5f3653fb7c59a3e181543b41f37b647663c4b6
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 02:20:34 -0800

    redefine function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return (None if idx < 0 or idx > (len(my_list) - 1)
-            else "{:d}".format(my_list[idx]))
+    l_length = len(my_list) - 1
+    if idx < 0 or idx > l_length:
+        return None
+    else:
+        return my_list[idx]
