f37f0748cb5c5309e1375b7739088cd7f56b6af2
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 09:47:03 -0800

    apply a workaround for a new edge case

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 def no_c(my_string):
     if my_string[:]:
-        new_str = my_string.translate({ord("c"): None})
-        new_string2 = new_str.translate({ord("C"): None})
-        return new_string2
+        new_str = ""
+        for ch in my_string:
+            new_str += ch if ord(ch) != 67 and ord(ch) != 99 else ''
+        return new_str
     return my_string
