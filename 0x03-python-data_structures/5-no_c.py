a2bc1c23d2d36ae28d0cfec573ae5e63d3353eed
Azeez Adebayo <debaycisse@gmail.com>
2024-01-08 07:24:20 -0800

    apply another workarround to the failing checks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     if my_string:
-        new_str = my_string.translate({ord('c'): None})
-        new_str = new_str.translate({ord('C'): None})
-        return new_str
+        new_str = my_string.translate({ord("c"): None})
+        new_str2 = new_str.translate({ord("C"): None})
+        return new_str2
     return my_string
