eba93cae008675afd2c0e7b8a5723676132e4c06
Azeez Adebayo <debaycisse@gmail.com>
2024-01-09 14:17:47 +0100

    use generator expression to use less lines

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,29 +1,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
-    n_tuple_a, n_tuple_b = (0, 0), (0, 0)
-    if len(tuple_a) == 2 and len(tuple_b) == 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    if len(tuple_a) == 0:
-        pass
-    elif len(tuple_a) == 1:
-        if tuple_a[0]:
-            n_tuple_a = (tuple_a[0], 0)
-        else:
-            n_tuple_a = (0, tuple_a[1])
-    elif len(tuple_a) > 2:
-        n_tuple_a = (tuple_a[0], tuple_a[1])
-    else:
-        n_tuple_a = (tuple_a[0], tuple_a[1])
-
-    if len(tuple_b) == 0:
-        pass
-    elif len(tuple_b) == 1:
-        if tuple_b[0]:
-            n_tuple_b = (tuple_b[0], 0)
-        else:
-            n_tuple_b = (0, tuple_b[1])
-    elif len(tuple_b) > 2:
-        n_tuple_b = (tuple_b[0], tuple_b[1])
-    else:
-        n_tuple_a = (tuple_a[0], tuple_a[1])
-
-    return (n_tuple_a[0] + n_tuple_b[0], n_tuple_a[1] + n_tuple_b[1])
+    a = tuple(tuple_a[i] if i < len(tuple_a) else 0 for i in range(2))
+    b = tuple(tuple_b[i] if i < len(tuple_b) else 0 for i in range(2))
+    return tuple(a[i] + b[i] for i in range(2))
