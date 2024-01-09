c736386f78bbee4156195b3ee7edc1fde6a1b414
Azeez Adebayo <debaycisse@gmail.com>
2024-01-09 14:28:27 +0100

    apply another algorithm to solve task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple(tuple_a[i] if i < len(tuple_a) else 0 for i in range(2))
-    b = tuple(tuple_b[i] if i < len(tuple_b) else 0 for i in range(2))
-    return tuple(a[i] + b[i] for i in range(2))
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    c = a[0] + b[0], a[1] + b[1]
+    return c
