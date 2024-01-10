05117fdbf14926a1e3144b28be8f39ae91f73d7e
Azeez Adebayo <debaycisse@gmail.com>
2024-01-10 14:35:12 -0800

    apply workaround to pass checker

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     a_dict = a_dictionary.copy()
-    if a_dict:
-        for k, v in a_dict.items():
-            a_dict.update({k: v*2})
-        return a_dict
+    for k, v in a_dict.items():
+        a_dict.update({k: v*2})
     return a_dict
