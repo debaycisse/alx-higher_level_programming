3de811668e886d6814db16116612d8eb6b8a9ef5
Azeez Adebayo <debaycisse@gmail.com>
2024-01-22 15:12:21 +0100

    implement task 0
    prints x elements of a list and return the number of
    elements printed using try and except clauses

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    treated = 0
+    i = 0
+    try:
+        while i < x:
+            print(f"{my_list[i]}", end = "")
+            treated += 1
+            i += 1
+        print("")
+        return (treated)
+    except IndexError:
+        print("")
+        return (treated)
+
