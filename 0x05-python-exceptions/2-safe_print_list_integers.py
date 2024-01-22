261e775cc35ff908e8b441c1d457db0e3f836ade
Azeez Adebayo <debaycisse@gmail.com>
2024-01-22 13:08:35 -0800

    implement task 2
    try / except in a loop and except the inner try via rasing of error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,19 +2,15 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     treated = 0
-    try:
-        while (i < x):
-            print("{:d}".format(list[i]), end="")
-            treated += 1
+    while (i < x):
+        try:
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                treated += 1
+                i += 1
+            except ValueError as e1:
+                raise TypeError from None
+        except TypeError as e2:
             i += 1
-        print("")
-        return (treated)
-    except TypeError:
-        while (i < x):
-            i += 1
-            print("{:d}".format(list[i]), end="")
-            treated += 1
-        print("")
-        return (treated)
-    except IndexError:
-        IndexError("list index out of range")
+    print("")
+    return (treated)
