eb613394ba4f3cfaaca473414df0ebdc1d49dd4b
Azeez Adebayo <debaycisse@gmail.com>
2024-01-22 15:29:31 +0100

    implement task 1 and correct task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i < x:
-            print(f"{my_list[i]}", end = "")
+            print(f"{my_list[i]}", end="")
             treated += 1
             i += 1
         print("")
