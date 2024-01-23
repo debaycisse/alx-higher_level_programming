d4c199ddab2a4937007298264e612a72fccb2801
Azeez Adebayo <debaycisse@gmail.com>
2024-01-23 15:02:53 +0100

    implement task 0
    create an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a real world square shape object.
+
+    It is an empty class currently
+    """
+
+    # This is an empty class, hence the below command
+    pass
