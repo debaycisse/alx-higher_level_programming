6ecb5a2669199332cff7f15df442e4dbba2bce81
Azeez Adebayo <debaycisse@gmail.com>
2024-01-23 15:28:57 +0100

    apply module level documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This module defines Square shape and its fields.
+
+It is going to define all attributes of Square shape with
+all possible operations that can be performed on it
+"""
+
+
 class Square:
     """Represents a real world square shape object.
 
