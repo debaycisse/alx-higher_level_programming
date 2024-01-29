0d79348ea28ceba9505b132ec3a45cd42f9b6df8
Azeez Adebayo <debaycisse@gmail.com>
2024-01-29 20:25:21 +0100

    implement task 6
    a class Rectangle that defines a rectangle by:
    
    1) Private instance attribute: width
    property def width(self): to retrieve it
    property setter def width(self, value): to set it
    
    2) Private instance attribute: height:
    property def height(self): to retrieve it
    property setter def height(self, value): to set it:
    
    3) Instantiation with optional width and height:
    def __init__(self, width=0, height=0):
    
    4) Public instance method: def area(self): that returns
    the rectangle area
    
    5) Public instance method: def perimeter(self): that returns
    the rectangle perimeter
    
    6) print() and str() should print the rectangle with the character #:
    
    7) repr() should return a string representation of the rectangle to be
    able to recreate a new instance by using eval()
    
    8) Print the message Bye rectangle...when an instance of
    Rectangle is deleted
    
    9) Public class attribute number_of_instances

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+"""This module houses a definition of a rectangle object,
+
+and those of its members, properties, and attributes
+"""
+
+
+class Rectangle:
+    """The Rectangle class where its attributes and properties are defined.
+    """
+
+    # This attribute keeps track of the number of object(s) of the class
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """this sets an initial value for the width, height, 
+        and the number_of_instances.
+        """
+        self.__height = height
+        self.__width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """This  method that returns the value of width
+
+        Returns:
+            the value of width is returned
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This method updates the value of the width.
+
+        Raises:
+            TypeError: if value is any type other than integer.
+            ValueError: if value is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """This  method that returns the value of width
+
+        Returns:
+            the value of width is returned
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This method updates the value of the height.
+
+        Raises:
+            TypeError: if value is any type other than integer.
+            ValueError: if value is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """This methods computes the area of a given instance of
+
+        a rectangle and returns the value.
+
+        Returns:
+            the computed value, obtained for the area.
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """This methods computes the perimeter of a given instance of
+
+        a rectangle and returns the value.
+
+        Returns:
+            the computed value, obtained for the perimeter.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        """This method gets called when the print() function or
+
+        the str() function is called on any instance of this class
+
+        Returns:
+            the rectangle shape via # character; using the width for
+            the number of the # to appear on a row and repeating the
+            row in a number of time of the value of the height
+        """
+        if self.__width != 0 and self.__height != 0:
+            i = 0
+            while i < self.__height:
+                j = 0
+                while j < self.__width:
+                    print("#", end="")
+                    j += 1
+                if i + 1 < self.__height:
+                    print()
+                i += 1
+        return ""
+
+    def __repr__(self):
+        """This method get called when a repr() function takes an
+
+        instance of this class as a parameter.
+
+        Returns:
+            the string formatted version or official representation
+            of the class which this instance belongs.
+        """
+        return (
+                "Rectangle({self.width}, {self.height})"
+                .format(self=self)
+               )
+
+    def __del__(self):
+        """This method handle the destructor aspect of an
+
+        instance of this class.
+        """
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
