6ebdf726ab44184d3906b314999d7d0926da0939
Azeez Adebayo <debaycisse@gmail.com>
2024-01-29 13:20:13 +0100

    implement task 0
    an empty Rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module houses a definition of a rectangle object,
+
+and those of its members, properties, and attributes
+"""
+
+
+class Rectangle:
+    """The Rectangle class where its attributes and properties are defined.
+
+    This class is empty for now.
+    """
+    pass
