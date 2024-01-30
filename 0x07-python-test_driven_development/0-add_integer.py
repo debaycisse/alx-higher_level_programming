329e6970ad1f55e24915dacfa097e2547ed078d1
Azeez Adebayo <debaycisse@gmail.com>
2024-01-30 17:10:07 +0100

    in progress

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+"""This module houses the definition of add_integer.
+
+A function that adds two integer values together and
+returns the result of this operation.
+"""
+
+def add_integer(a, b=98):
+    """This function computes the addition operation on both a and b
+    a and b are added and their result is returned
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
