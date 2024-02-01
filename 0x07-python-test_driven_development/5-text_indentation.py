8e3f11c668441dfbed0d58776c7f9f2bda97b13d
Azeez Adebayo <debaycisse@gmail.com>
2024-02-01 20:46:50 +0100

    implement task 4
    write function's text file with which the function's
    requirement is ensured and the function's definition

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+"""This is a module for the text_indentation function.
+
+This module contains the implementation of the
+programming logic for the function
+"""
+
+
+def text_indentation(text):
+    """This function prints a text with 2 new lines after
+    each of these characters: ., ? and :
+
+    Args:
+        text: this is the string to be operated on
+
+    Raises:
+        TypeError: this is raised if the text's type is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    scanned = ""
+    for ch in text:
+        if ch in ['.', ':', '?']:
+            scanned += ch
+            print("{}\n".format(scanned.strip()))
+            scanned = ""
+        else:
+            scanned += ch
+    print("{}".format(scanned.strip()), end="")
