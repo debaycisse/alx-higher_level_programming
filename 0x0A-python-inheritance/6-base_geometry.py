2517b9e398246b01ed29efe5485a536f47ddf99c
Azeez Adebayo <debaycisse@gmail.com>
2024-02-05 13:01:03 -0800

    implement task 6
    a class BaseGeometry, contains a method
    area that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This modules houses the definition of BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """This class defines the properties and methods of a Geometry
+    """
+
+    def area(self):
+        """area() method is to calculate the area of an instance of
+        a Geometry, but raises exception since it is not implemented yet
+        """
+        raise Exception("area() is not implemented")
