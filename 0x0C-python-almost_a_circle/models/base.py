7c5d5008144da114c05253da462fb0461b94edb8
Azeez Adebayo <debaycisse@gmail.com>
2024-02-11 19:06:43 +0100

    implement task 3
    add logic to validate values before updating attributes.
    add getters and setters for all necessary attributes.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def integer_validator(self, name, value):
+        """This methods validates a passed value and responds accordingly
+
+        Args:
+            name: the name of a given value at a point in time.
+            value: the value of the passed name.
+        """
+        if type(value).__name__ != 'int':
+            raise TypeError("{} must be an integer".format(name))
+        if name == "width" or name == "height":
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        elif name == "x" or name == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,8 +25,86 @@ class Rectangle(b.Base):
             x: the x-axis dimension of an instance
             y: the y-axis dimension of an instance
         """
-        super().__init__(id)
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
+        super().integer_validator("x", x)
         self.__x = x
+        super().integer_validator("y", y)
         self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """This method gets and returns a value of the width's attribute.
+
+        Returns:
+            The value of the width attribute of an instance.
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """This method updates the value of the width attribute.
+
+        Args:
+            value: the value to be used in updating the width attribute.
+        """
+        super().integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """This method returns the value of the height attribute.
+
+        Returns:
+            The value of the height attribute of an instance.
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """This method updates the height value of an instance of this class.
+
+        Args:
+            value: the value with which the value of the height is updated.
+        """
+        super().integer_validator("height", value)
+
+    @property
+    def x(self):
+        """This method gets and returns the value of the x attribute.
+
+        Returns:
+            The value of the x attribute of an instance of this class.
+        """
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """This method updates the value of the x attribute of an instance.
+
+        Args:
+            value: the value with which the value of the
+            x attribute is updated.
+        """
+        super().integer_validator("x", value)
+
+    @property
+    def y(self):
+        """This method obtains the value of the y attribute.
+
+        Returns:
+            The value of the y attribute of a given instance.
+        """
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """This method updates the value of the y attribute.
+
+        Args:
+            value: the value with which the value of y is updated.
+        """
+        super().integer_validator("y", value)
