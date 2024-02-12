d6fa9016893cfef781fdc94f816c08f86cbd7c05
Azeez Adebayo <debaycisse@gmail.com>
2024-02-12 23:05:50 +0100

    implement task 16
    add a class method that writes the JSON string
    respresentation of a list of objects of base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 along with its properties or attributes and methods"""
 
 
+import json
+
+
 class Base:
     """This is a class for the Base where its
     properties and methods are defined
@@ -56,5 +59,33 @@ class Base:
         if len(list_dictionaries) <= 0:
             return "[]"
         elif len(list_dictionaries) > 0:
-            import json
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """This method writes the JSON string representation of
+        list_objs to a file.
+
+        Args:
+            cls: the class argument to indicate that this is a class method.
+            list_objs: list of instances of this class
+
+        Returns:
+            A JSON equivalent of the list of the string representation
+            of the instances of this class.
+        """
+        if len(list_objs) == 0:
+            with open(
+                      "{}.json".format(cls.__name__),
+                      "w", encoding="utf-8"
+                     ) as file_:
+                file_.write("[]")
+        elif len(list_objs) > 0:
+            with open(
+                      "{}.json".format(cls.__name__),
+                      "w", encoding="utf-8"
+                     ) as file_:
+                data = list()
+                for obj in list_objs:
+                    data.append(obj.to_dictionary())
+                file_.write(cls.to_json_string(data))
