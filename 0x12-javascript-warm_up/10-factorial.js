758dee113e45e64ea767ee02aedd3e9bfcef9741
Azeez Adebayo <debaycisse@gmail.com>
2024-04-08 12:30:09 -0700

    implement task 10. Factorial
    script takes a single argument, a number of course whose
    factorial result is printed on the screen

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial (num) {
+    if (!isNaN(num)) {
+	if (num === 0) {
+	    return 1;
+	} else {
+	    return (num * factorial(num - 1));
+	}
+    } else {
+	return 1;
+    }
+}
+console.log(factorial(parseInt(process.argv[2])));
+	
