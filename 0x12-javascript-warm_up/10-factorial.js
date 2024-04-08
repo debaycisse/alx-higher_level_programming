b806ed8755220bc996c09e85b6333d99af8894c7
Azeez Adebayo <debaycisse@gmail.com>
2024-04-08 12:39:08 -0700

    apply semistandard compliance on file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 function factorial (num) {
-    if (!isNaN(num)) {
-	if (num === 0) {
-	    return 1;
-	} else {
-	    return (num * factorial(num - 1));
-	}
+  if (!isNaN(num)) {
+    if (num === 0) {
+      return 1;
     } else {
-	return 1;
+      return (num * factorial(num - 1));
     }
+  } else {
+    return 1;
+  }
 }
 console.log(factorial(parseInt(process.argv[2])));
-	
