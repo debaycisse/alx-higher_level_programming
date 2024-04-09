a8038e68cfc91e1f0fa1043f0e23530d95ab03b8
Azeez Adebayo <debaycisse@gmail.com>
2024-04-09 08:33:56 -0700

    implement task 6. Square #1
    script contains a class, named Square, which:
            - takes one parameter
            - inherits from another class, named Square
            - contains a public method, named charPrint, which:
                    - prints a shape, using its given parameter or 'X' if no
                    parameter is given.

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Sq = require('./5-square');
+class Square extends Sq {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let squareFill = '';
+      for (let i = 0; i < this.width; i++) {
+        squareFill += c;
+      }
+      for (let j = 0; j < this.height; j++) {
+        console.log(squareFill);
+      }
+    }
+  }
+}
+module.exports = Square;
