dbfff42bed0578a2582bec7917b2258b2049f2d1
Azeez Adebayo <debaycisse@gmail.com>
2024-04-10 08:08:12 -0700

    update logic to use less variable

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const fileSystem = require('node:fs');
-
-const fileSources = [process.argv[2], process.argv[3], process.argv[4]];
-
-for (let i = 0; i < 2; i++) {
-  fileSystem.readFile(fileSources[i], 'utf8', function (error, content) {
+for (let i = 2; i < 4; i++) {
+  fileSystem.readFile(process.argv[i], 'utf8', function (error, content) {
     if (error) {
       console.log(error);
     } else {
-      fileSystem.appendFile(fileSources[2], content + '\n', function (error) {
+      fileSystem.appendFile(process.argv[4], content + '\n', function (error) {
         if (error) {
           console.log(error);
         }
