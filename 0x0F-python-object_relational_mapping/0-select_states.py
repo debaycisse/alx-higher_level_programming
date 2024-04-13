7f365dac403ab8af55b93a7df5311e69cd6949b4
Azeez Adebayo <debaycisse@gmail.com>
2024-04-13 20:49:19 +0100

    implement task 0. Get all states
    script that lists all states from the database
    hbtn_0e_0_usa using MySQLdb module

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module uses MySQLdb to list out all states
+in hbtn_0e_0_usa database"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id")
+    for row in cur.fetchall():
+        print(row)
