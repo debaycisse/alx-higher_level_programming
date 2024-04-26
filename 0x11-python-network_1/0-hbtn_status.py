84d9f593b31119ef88bbaf49f67d817d5adfa50b
Azeez Adebayo <debaycisse@gmail.com>
2024-04-26 13:53:40 -0700

    refactor the code base to use less lines

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module fetches data from a url, using the urllib module"""
 if __name__ == '__main__':
-    import urllib.request
+    from urllib.request import urlopen
 
-    with urllib\
-         .request\
-         .urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: ', type(html))
-        print('\t- content: ', html)
-        print('\t- utf8 content: ', response.reason)
+        print(
+            'Body response:\n\t- type: {}\
+             \n\t- content: {}\n\t- utf8 content: {}'.format(
+                 type(html), html, response.reason
+             )
+        )
