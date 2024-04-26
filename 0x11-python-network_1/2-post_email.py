fd2bc34c03eaeff81596e6669c7f7ddf46dcb830
Azeez Adebayo <debaycisse@gmail.com>
2024-04-26 17:06:55 +0100

    work in progress - task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the
+body of the response (decoded in utf-8)"""
+
+if __name__ == '__main__':
+    # post_email hr@holbertonschool.com   https://httpbin.org/post
+    import urllib.request
+    import urllib.parse
+    from sys import argv
+
+    data = urllib.parse.urlencode({'email': argv[2]})
+    _data = data.encode('ascii')
+
+    with urllib\
+         .request\
+         .urlopen('https://httpbin.org/post', data=_data) as response:
+#        print(dir(response.read()))
+        print(response.read().decode('utf-8').split('\n'))
+        
