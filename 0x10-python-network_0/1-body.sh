4f553c778b1580e1a4ab6dc6ea05328aa87491f8
Azeez Adebayo <debaycisse@gmail.com>
2024-04-27 12:16:32 -0700

    work in progress

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script takes in a URL, sends a GET request to it, and displays the body of the response
+if [ "$(curl -s -o /dev/null -w '%{http_code}' \"$1\")" == 200 ]; then curl -s "$1" else echo ""
+fi
