5f920eb116bf3fc899f60d925d226898a6e42617
Azeez Adebayo <debaycisse@gmail.com>
2024-04-27 20:51:44 +0100

    add the test in a string

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL, sends a GET request to it, and displays the body of the response
-if [ "$(curl -s -o /dev/null -w '%{http_code}' """$1""")" == 200 ]; then curl -s "$1"; export WINEARCH=win32; fi
+if [ "$(curl -s -o /dev/null -w '%{http_code}' """$1""")" == "200" ]; then curl -s "$1"; export WINEARCH=win32; fi
