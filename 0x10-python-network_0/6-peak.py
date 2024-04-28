b991f84c4f0550de18941bf9ca107e0288e7384c
Azeez Adebayo <debaycisse@gmail.com>
2024-04-28 10:42:20 -0700

    apply new algorithm to find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,18 @@
 def find_peak(list_of_integers):
     """The find_peak function obtains the peak,
     among the elements of the passed integer list"""
-    if len(list_of_integers) < 1:
+    if len(list_of_integers) == 0:
         return None
-    l_copy = list_of_integers.copy()
-    l_copy.sort()
-    return l_copy[-1]
+    if len(list_of_integers) > 2:
+        split_half = len(list_of_integers) // 2
+        a = find_peak(list_of_integers[:split_half + 1])
+        b = find_peak(list_of_integers[split_half + 1:])
+    else:
+        if len(list_of_integers) == 2:
+            if list_of_integers[0] > list_of_integers[1]:
+                return list_of_integers[0]
+            return list_of_integers[1]
+        return list_of_integers[0]
+    if a > b:
+        return a
+    return b
