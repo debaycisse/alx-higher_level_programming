eab137d40f1441ca47115ae2a56009fdd8d423f9
Azeez Adebayo <debaycisse@gmail.com>
2024-05-21 16:36:31 +0100

    correct an error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
     console.log('code:', response.statusCode);
   }
