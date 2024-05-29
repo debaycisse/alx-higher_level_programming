a467c1ca364038c307eead7abc9b726662cb6f0f
Azeez Adebayo <debaycisse@gmail.com>
2024-05-29 15:49:15 +0100

    implement task 0
    script updates the text color of
    the <header> element

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+//  script that updates the text color of the <header> element to red (#FF0000)
+const myHeader = document.querySelector('header');
+myHeader.style.color = '#FF0000';
